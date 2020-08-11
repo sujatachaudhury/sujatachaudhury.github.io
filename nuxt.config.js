2f291163afb7c7d2abdeb6ae45804fd2071d4023
sujatachaudhury <44702020+sujatachaudhury@users.noreply.github.com>
2020-08-11 12:21:44 +0530

    fix: router

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ export default {
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
+  router: {
+    base: '/'
+  },
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
